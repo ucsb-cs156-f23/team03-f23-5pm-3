5c869161ad9adff44d472be5ea5459002722b515
wyatthspivak <wyattspivak@ucsb.edu>
2023-11-07 10:48:49 -0800

    added articles fixtures for testing

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,40 @@
+const articlesFixtures = {
+    oneArticles: [
+        {
+            "id": 0,
+            "title": "HowTo",
+            "url": "localhost.com",
+            "explanation": "How to do something",
+            "email": "local.host@localhost.com",
+            "dateAdded": "2023-11-07T18:23:08.090Z"
+        }
+    ],
+    threeArticles: [
+        {
+            "id": 0,
+            "title": "HowTo",
+            "url": "localhost.com",
+            "explanation": "How to do something",
+            "email": "local.host@localhost.com",
+            "dateAdded": "2023-11-07T18:23:08.090Z"
+        },
+        {
+            "id": 1,
+            "title": "HowTo",
+            "url": "localhost.com",
+            "explanation": "How to do something",
+            "email": "local.host@localhost.com",
+            "dateAdded": "2023-11-07T18:23:08.090Z"
+        },
+        {
+            "id": 2,
+            "title": "HowTo",
+            "url": "localhost.com",
+            "explanation": "How to do something",
+            "email": "local.host@localhost.com",
+            "dateAdded": "2023-11-07T18:23:08.090Z"
+        }
+    ]
+};
+
+export {articlesFixtures};
