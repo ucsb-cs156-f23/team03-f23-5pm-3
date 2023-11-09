6b3bbbc1cb1ba2f69446d20636a7786af6456a66
Kaitlyn t <kaitlynt@169-231-21-114.wireless.ucsb.edu>
2023-11-09 01:19:20 -0800

    cry

---
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -0,0 +1,67 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBOrganizationUtils"
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function UCSBOrganizationTable({ organization, currentUser }) {
+
+    const navigate = useNavigate();
+
+    const editCallback = (cell) => {
+        navigate(`/ucsborganization/edit/${cell.row.values.orgcode}`)
+    }
+
+    // Stryker disable all : hard to test for query caching
+
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/ucsborganization/all"]
+    );
+    // Stryker restore all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+
+    const columns = [
+        {
+            Header: 'Organization Code',
+            accessor: 'orgcode', // accessor is the "key" in the data
+        },
+        {
+            Header: 'Organization Translation Short',
+            accessor: 'orgTranslationShort',
+        },
+        {
+            Header: 'Organization Translation',
+            accessor: 'orgTranslation',
+        },
+        {
+            Header: 'Inactive',
+            accessor: 'inactive',
+        }
+    ];
+
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        columns.push(ButtonColumn("Edit", "primary", editCallback, "UCSBOrganizationTable"));
+        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "UCSBOrganizationTable"));
+    } 
+    organization.forEach((element, index, arr) => {
+        console.log(element);
+        if (arr[index]['inactive'] === true){
+            arr[index]['inactive'] = "true";
+        }
+        if (arr[index]['inactive'] === false){
+            arr[index]['inactive'] = "false";
+        }
+    });
+    return <OurTable
+        data={organization}
+        columns={columns}
+        testid={"UCSBOrganizationTable"}
+    />;
+};
